0bd9d31d7d300968e64872ef2a28ea4570e4c76b
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-05-18 22:12:24 +0300

    simplify pointer

--- tslab_test.go
+++ tslab_test.go
@@ -9,7 +9,9 @@
 package tslab_test
 
 import (
+	"sync"
 	"testing"
+	"unsafe"
 
 	"github.com/fogfish/tslab"
 )
@@ -20,10 +22,9 @@ type T struct {
 }
 
 func BenchmarkAlloc(b *testing.B) {
-	b.ReportAllocs()
-
 	heap := tslab.New[T](16 * 1024)
 
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		heap.Alloc()
 	}
@@ -32,8 +33,50 @@ func BenchmarkAlloc(b *testing.B) {
 func BenchmarkAllocFree(b *testing.B) {
 	heap := tslab.New[T](16 * 1024)
 
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		p, _ := heap.Alloc()
 		heap.Free(p)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	heap := tslab.New[T](16 * 1024)
+	p, _ := heap.Alloc()
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		heap.Get(p)
+	}
+}
+
+var t *T
+
+func BenchmarkSliceGet(b *testing.B) {
+	heap := make([]T, 16*1024, 16*1024)
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		t = &heap[i%16*1024]
+	}
+}
+
+func BenchmarkSliceUnsafe(b *testing.B) {
+	heap := make([]byte, 16*1024, 16*1024)
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		t = (*T)(unsafe.Pointer(&heap[1024]))
+	}
+}
+
+func BenchmarkSyncPool(b *testing.B) {
+	pool := sync.Pool{}
+	pool.New = func() any { return T{} }
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		obj := pool.Get()
+		pool.Put(obj)
 	}
 }
